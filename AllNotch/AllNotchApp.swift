ba2a65d4483c23d5d39de9a58b7d4cc93861323e
thib-crypt <pyapp.encrypted@gmail.com>
2026-06-06 19:50:32 +0200

    Initial Commit

---
+++ AllNotch/AllNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllNotchApp.swift
+//  AllNotch
+//
+//  Created by Thibault Thévenin on 06/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct AllNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllNotchTests/AllNotchTests.swift
@@ -0,0 +1,17 @@
+//
+//  AllNotchTests.swift
+//  AllNotchTests
+//
+//  Created by Thibault Thévenin on 06/06/2026.
+//
+
+import Testing
+@testable import AllNotch
+
+struct AllNotchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
